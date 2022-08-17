1462b714b5b6fecc8a527b907f314f1ca0c95a09
SudhansuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-08-17 23:00:36 +0530

    added auth route

--- api/index.js
+++ api/index.js
@@ -1,3 +1,30 @@
+const PORT = 8080 || process.env.PORT;
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const multer = require("multer");
+const path = require("path");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/auth", authRoute);
+
+app.listen(PORT, () => {
+  console.log(`server started on PORT ${PORT}`);
+});
