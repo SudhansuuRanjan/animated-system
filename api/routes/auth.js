f1069dd645c8009803e2f66c8c7ac5b9e864795e
SudhansuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-08-18 01:46:39 +0530

    fixed bugs

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -24,10 +24,10 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong credentials1!");
+    !user && res.status(400).json("Wrong credentials!");
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong credentials2!");
+    !validated && res.status(400).json("Wrong credentials!");
 
     const { password, ...others } = user._doc;
     res.status(200).json(others);
